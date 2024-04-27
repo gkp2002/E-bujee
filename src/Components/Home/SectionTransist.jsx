3b15cd4d7b6965cb365de2385b582ce2210051f9
gkp2002 <gajanank015@gmail.com>
2024-04-27 17:49:29 +0530

    Service page Editing

--- src/Components/Home/SectionTransist.jsx
+++ src/Components/Home/SectionTransist.jsx
@@ -15,35 +15,35 @@ function SectionTransist() {
                    <li className="md:w-1/5 w-full p-10 "> 
                     <div className=' gap-6 flex flex-col justify-center items-center py-20 rounded-xl px-2 cursor-pointer hover:bg-yellow-600' data-aos="fade-up" data-aos-duration="1000" data-loaded="true">
                           <img src="/src/assets/seo.png" alt="" className='w-20'/>
-                          <h3 className='text-4xl font-bold '>Web Design</h3>
-                          <p className='text-center mb-4 mt-0'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quo deleniti similique laudantium!</p>
+                          <h3 className='text-4xl text-center font-bold '>Web Design</h3>
+                          <p className='text-center mb-4 mt-0 text-xl'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quo deleniti similique laudantium!</p>
                     </div>
                    </li>
                    <li className="md:w-1/5 w-full p-10 ">
                    <div className=' gap-6 flex flex-col justify-center items-center py-16 rounded-xl px-4 cursor-pointer hover:bg-yellow-600' data-aos="fade-up" data-aos-duration="1000" data-loaded="true">
                          <img src="/src/assets/seo.png" alt="" className='w-20'/>
-                         <h3 className='text-4xl font-bold'>Web Design</h3>
-                         <p className='text-center mb-4 mt-0 '>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quo deleniti similique laudantium!</p>
+                         <h3 className='text-4xl text-center font-bold'>Web Design</h3>
+                         <p className='text-center mb-4 mt-0 text-xl'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quo deleniti similique laudantium!</p>
                    </div>
                   </li>
                   <li className="md:w-1/5 w-full p-10 ">
                   <div className=' gap-6 flex flex-col justify-center items-center py-16 rounded-xl px-4 cursor-pointer hover:bg-yellow-600' data-aos="fade-up" data-aos-duration="1000" data-loaded="true">
                         <img src="/src/assets/seo.png" alt="" className='w-20'/>
-                        <h3 className='text-4xl font-bold'>Web Design</h3>
-                        <p className='text-center mb-4 mt-0  '>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quo deleniti similique laudantium!</p>
+                        <h3 className='text-4xl text-center font-bold'>Web Design</h3>
+                        <p className='text-center mb-4 mt-0 text-xl '>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quo deleniti similique laudantium!</p>
                   </div>
                  </li>
                  <li className="md:w-1/5 w-full p-10 ">
                  <div className=' gap-6 flex flex-col justify-center items-center py-16 rounded-xl px-4 cursor-pointer hover:bg-yellow-600' data-aos="fade-up" data-aos-duration="1000" data-loaded="true">
                        <img src="/src/assets/seo.png" alt="" className='w-20'/>
-                       <h3 className='text-4xl font-bold'>Web Design</h3>
-                       <p className='text-center mb-4 mt-0   '>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quo deleniti similique laudantium!</p>
+                       <h3 className='text-4xl text-center font-bold'>Web Design</h3>
+                       <p className='text-center mb-4 mt-0 text-xl  '>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quo deleniti similique laudantium!</p>
                  </div>
                 </li>
                 <li className="md:w-1/5 w-full p-10 ">
                 <div className=' gap-6 flex flex-col justify-center items-center py-16 rounded-xl px-4 cursor-pointer hover:bg-yellow-600' data-aos="fade-up" data-aos-duration="1000" data-loaded="true">
                       <img src="/src/assets/seo.png" alt="" className='w-20'/>
-                      <h3 className='text-4xl font-bold'>Web Design</h3>
+                      <h3 className='text-4xl text-center font-bold'>Web Design</h3>
                       <p className='text-center mb-4 mt'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quo deleniti similique laudantium!</p>
                 </div>
                </li>
